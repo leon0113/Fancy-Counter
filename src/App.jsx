a8c16d69553fd6ee89daca5e6c99ce372496707d
Leon <tahjib.leon399@gmail.com>
2024-03-21 06:19:39 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <div>
+        <h1>Hello world</h1>
+        <p>0</p>
+        <button>Reset</button>
+        <div>
+          <button>-</button>
+          <button>+</button>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
